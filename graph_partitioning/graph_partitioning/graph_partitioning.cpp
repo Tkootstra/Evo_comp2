6b0d5acabe121d7e8075ac917a81438a774f16d6
Timo <timokootstr@msn.com>
2020-03-02 13:32:10 +0100

    kek

---
+++ graph_partitioning/graph_partitioning/graph_partitioning.cpp
@@ -0,0 +1,67 @@
+﻿// graph_partitioning.cpp : Defines the entry point for the application.
+//
+
+#include "graph_partitioning.h"
+#include <iostream>
+#include <list>
+#include <vector>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	
+	cout << "Hello CMake." << endl;
+	std::ifstream dataFile("Graph500.txt");
+	std::string line;
+	while (std::getline(dataFile, line))
+	{
+		cout << line << endl;
+	}
+
+
+
+	// hier data inladen -> Graph500.txt
+	return 0;
+}
+
+
+class Node
+{
+public:
+	int indexLocation;
+	std::list<int> ConnectionLocations;
+	Node(int indexLoc, std::list<int> ConLocations)
+	{
+		indexLocation = indexLoc;
+		ConnectionLocations = ConLocations;
+	}
+};
+
+class Solution 
+{
+	
+public:
+	std::vector<int> valueVector;
+	int values[];
+	
+
+	Solution(int values[])
+	{	
+		values = values;
+		valueVector.insert(valueVector.begin(), values, values + (sizeof(values) / sizeof(*values)));
+	}
+};
+
+
+
+
+
+
+
+
