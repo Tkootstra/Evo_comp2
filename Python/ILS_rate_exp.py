5c8864024c94029089ab3d83b2334678a77d4ee8
Timo <timokootstr@msn.com>
2020-04-01 19:57:25 +0200

    new ILS rate exp data

--- Python/ILS_rate_exp.py
+++ Python/ILS_rate_exp.py
@@ -4,24 +4,30 @@ import numpy as np
 from multiprocessing import Pool
 from itertools import repeat
 import pandas as pd
-MAX_ITER = 10000
+MAX_ITER = 5000
 nodes = search_algos.parse_graph()
 
-rates = list(np.linspace(0.001, 0.01, num=10))
+rates = list(np.arange(0.0, 0.04, step=0.004))
 rates = [float(x) for x in rates]
+n_repeats = 5
+
 
 pool = Pool(10)
-
+total_res = []
 args = list(zip(repeat(nodes, 10), repeat(MAX_ITER, 10), rates))
-ILS_iter_res12 = pool.starmap(search_algos.ILS_iter, args)
-results = []
-for i, dict_ in enumerate(ILS_iter_res12):
-    dict_['iteration'] = repeat(i, len(dict_[list(dict_.keys())[0]]))
-    dict_['rate'] = repeat(rates[i], len(dict_[list(dict_.keys())[0]]))
-    results.append(pd.DataFrame(dict_))
-results_total = pd.concat(results)
-results_total.to_csv('ILS_rate_exp.csv')
 
-
+for x in range(n_repeats):
+    ILS_iter_res12 = pool.starmap(search_algos.ILS_iter, args)
+    results = []
+    for i, dict_ in enumerate(ILS_iter_res12):
+        dict_['iteration'] = repeat(x, len(dict_[list(dict_.keys())[0]]))
+        dict_['rate'] = repeat(rates[i], len(dict_[list(dict_.keys())[0]]))
+        results.append(pd.DataFrame(dict_))
+    results_total = pd.concat(results)
+    total_res.append(results_total)
+all_ = pd.concat(total_res)
+all_.to_csv('ILS_rate_exp.csv')
+    
+    
 
     
